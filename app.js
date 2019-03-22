b39e970b2d5aa7a6b278092a15667e8b6a7f6501
Tao Ning <ningtaous@gmail.com>
2019-03-22 08:17:42 -0400

    Config project
    1. Add stylelint to pre-commit
    2. Fix scss not compile properly

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(
 	sassMiddleware({
 		src: path.join(__dirname, 'public'),
 		dest: path.join(__dirname, 'public'),
-		indentedSyntax: true, // true = .sass and false = .scss
+		indentedSyntax: false, // true = .sass and false = .scss
 		sourceMap: true,
 	})
 );

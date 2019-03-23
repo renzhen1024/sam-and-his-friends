ac529191f9599ecae28131943dabb922505b2df0
Tao Ning <ningtaous@gmail.com>
2019-03-23 07:31:28 -0400

    Use faker for string in post

--- data/post.js
+++ data/post.js
@@ -1,13 +1,15 @@
+const fakerZh = require('faker-zh-cn');
+const faker = require('faker');
+
 module.exports = {
-	title: 'Magna sed adipiscing',
-	subTitle: 'Lorem ipsum dolor amet nullam consequat etiam feugiat',
-	date: 'November 1, 2015',
-	author: 'Jane Doe',
-	authorImageUrl: 'images/avatar.jpg',
-	imageUrl: 'images/pic01.jpg',
-	content:
-		'Mauris neque quam, fermentum ut nisl vitae, convallis maximus nisl. Sed mattis nunc id lorem euismod placerat. Vivamus porttitor magna enim, ac accumsan tortor cursus at. Phasellus sed ultricies mi non congue ullam corper. Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.',
-	category: 'General',
-	numLikes: 28,
-	numComments: 128,
+	title: fakerZh.Name.findName(),
+	subTitle: fakerZh.Lorem.sentences(),
+	date: faker.date.past(),
+	author: fakerZh.Name.findName(),
+	authorImageUrl: faker.image.avatar(),
+	imageUrl: faker.image.image(),
+	content: fakerZh.Lorem.paragraphs(),
+	category: fakerZh.Name.findName(),
+	numLikes: faker.random.number(),
+	numComments: faker.random.number(),
 };

615d6aa22d7be87dd36b29191839696d3fd0df21
Tao Ning <ningtaous@gmail.com>
2019-03-23 23:57:18 -0400

    add query from discourse

---
+++ data/formatters/post.js
@@ -0,0 +1,20 @@
+const config = include('config');
+
+function postFormatter({ data = {} } = {}) {
+	const { topics } = data.topic_list;
+	return topics.map(topic => ({
+		title: topic.title,
+		subTitle: topic.fancy_title || '这张帖子没有副标题',
+		date: topic.bumped_at,
+		name: '小山',
+		authorImageUrl:
+			'https://renzhen1024.com/user_avatar/renzhen1024.com/mountainsun1988/240/90_2.png',
+		content: topic.excerpt || '内容被山姆哥藏起来了，点击“继续阅读”',
+		category: '山姆哥',
+		numLikes: topic.like_count,
+		numComments: topic.posts_count,
+		isReaderMode: config.isReaderMode,
+	}));
+}
+
+exports.postFormatter = postFormatter;
--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,32 @@
 const express = require('express');
+const axios = require('axios');
 
-const { createPosts } = include('data/post');
-const { createMiniPosts } = include('data/mini-post');
+const { createPosts } = include('data/mocks/post');
+const { createMiniPosts } = include('data/mocks/mini-post');
+const { postFormatter } = include('data/formatters/post');
 const config = include('config');
 
 const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res) => {
-	const posts = createPosts(3);
-	const miniPosts = createMiniPosts(4);
-	const postList = createMiniPosts(6);
-	res.render('index', { posts, miniPosts, postList, config });
+	let posts;
+	let miniPosts;
+	let postList;
+
+	if (process.env.IS_TEST) {
+		posts = createPosts(3);
+		miniPosts = createMiniPosts(4);
+		postList = createMiniPosts(6);
+		res.render('index', { posts, miniPosts, postList, config });
+	} else {
+		axios.get('https://renzhen1024.com/c/7.json').then(response => {
+			posts = postFormatter(response);
+			miniPosts = createMiniPosts(4);
+			postList = createMiniPosts(6);
+			res.render('index', { posts, miniPosts, postList, config });
+		});
+	}
 });
 
 module.exports = router;

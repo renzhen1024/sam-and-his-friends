ae7efa8ed7fc608bfa838a228c402e68f21eb1d9
Tao Ning <ningtaous@gmail.com>
2019-03-23 21:30:46 -0400

    add footer and about component

--- config.js
+++ config.js
@@ -6,4 +6,21 @@ module.exports = {
 		profileImage: 'images/logo.jpg',
 	},
 	isReaderMode: true,
+	about:
+		'篇也解十柏版俗于语二三育论科宝文,行举教通世子阳评子位语宝命,语语出译货编手国考国不通八,一相间结语省',
+	socialMedias: [
+		{ name: 'Facebook', className: 'facebook', url: 'http://facebook.com' },
+		{ name: 'Twitter', className: 'twitter', url: 'http://twitter.com' },
+		{
+			name: 'Instagram',
+			className: 'instagram',
+			url: 'https://www.instagram.com',
+		},
+		{ name: 'RSS', className: 'rss', url: 'https://rss.com/' },
+		{
+			name: 'Email',
+			className: 'envelope',
+			url: 'mailto:mountainsun1988@gmail.com',
+		},
+	],
 };
--- locale/zh-cn.js
+++ locale/zh-cn.js
@@ -3,4 +3,6 @@ module.exports = {
 	'i18n-post-category': '分类: %{category}',
 	'i18n-pagination-previous-page': '上一页',
 	'i18n-pagination-next-page': '下一页',
+	'i18n-sidebar-about': 'About',
+	'i18n-sidebar-learn-more': '了解更多',
 };

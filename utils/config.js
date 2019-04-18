bc49b23000505c26ec930ada118be146f51d34a6
Tao Ning <mi.tao.code@gmail.com>
2019-04-18 16:54:19 -0400

    fix 404, add back profile image

--- utils/config.js
+++ utils/config.js
@@ -7,7 +7,7 @@ exports.creator = {
 	firstName: '江山',
 	lastName: '孙',
 	description: '论师义后着语开注固解关字几,汉尊之语典相帝学年家相而',
-	profileImage: 'images/logo.jpg',
+	profileImage: 'images/sam-site-profile-image.jpg',
 };
 
 exports.lengthOfActiveUsersAtIndexPage = 5;

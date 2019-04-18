eec35d74952fc9e47b0d6c5f400c145e46a755f0
Tao Ning <mi.tao.code@gmail.com>
2019-04-18 16:43:38 -0400

    Add request interceptor for Axois to log out full url

--- data/requests/request.js
+++ data/requests/request.js
@@ -1,4 +1,17 @@
 const axios = require('axios');
+const debug = require('debug')('sam-and-his-friends:request');
+
+// Add request interceptors to log out full url
+axios.interceptors.request.use(function getFullUrlFromRequest(config) {
+	const { params, url } = config;
+	const queryParamArray = Object.entries(params);
+	const fullUrl = queryParamArray.reduce(
+		(acc, [key, value]) => `${acc}&${key}=${value}`,
+		queryParamArray.length === 0 ? url : `${url}?`
+	);
+	debug(fullUrl);
+	return config;
+});
 
 const { DOMAIN } = include('utils/constants');
 

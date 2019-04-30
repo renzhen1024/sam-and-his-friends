94d95e3bd3e3d4114674ca0e6485c683408f90dc
Tao Ning <mi.tao.code@gmail.com>
2019-04-29 23:17:58 -0400

    Re-enable CI
    Also, update icon and webmanifest

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@
 echo "start travis ci deploy"
 echo "ssh to server..."
 
-ssh -o "StrictHostKeyChecking no" root@68.183.103.112 "cd /usr/share/nginx/sam-and-his-friends && pm2 stop 1 && git pull && yarn install && npm run build && NODE_ENV=production pm2 start ./server.js"
+ssh -o "StrictHostKeyChecking no" root@68.183.103.112 "su - tningjs && cd sam-and-his-friends && pm2 stop 0 && git pull && yarn install && npm run build && NODE_ENV=production pm2 start ./server.js"
 
 echo "deploy success"

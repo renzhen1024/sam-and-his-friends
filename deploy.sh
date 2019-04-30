e84fe22afe93c1f62a098412ad27c079367d4e31
Tao Ning <mi.tao.code@gmail.com>
2019-04-29 23:28:06 -0400

    Update CI

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@
 echo "start travis ci deploy"
 echo "ssh to server..."
 
-ssh -o "StrictHostKeyChecking no" root@68.183.103.112 "cd /home/tningjs/sam-and-his-friends && pm2 stop 0 && git pull && yarn install && npm run build && NODE_ENV=production pm2 start ./server.js"
+ssh -o "StrictHostKeyChecking no" root@68.183.103.112 "cd /home/tningjs/sam-and-his-friends && pm2 stop 2 && git pull && yarn install && npm run build && NODE_ENV=production pm2 start ./server.js"
 
 echo "deploy success"
